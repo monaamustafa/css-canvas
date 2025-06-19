3bc917c563e9cde74a2505295dc9ba437d9e548f
mona.abdelmalek <mona.abdelmalek@cyshield.com>
2025-06-20 02:50:24 +0300

    fix canvas

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext('2d');
 // Set canvas dimensions
 canvas.width = window.innerWidth;   
 canvas.height = window.innerHeight;
+let hue=0
 // Resize canvas on window resize
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
@@ -14,39 +15,64 @@ const mouse = {
     x: 0,
     y: 0
 };
+canvas.addEventListener('click', (event) => {
+   
+init(20)
+
+});
 canvas.addEventListener('mousemove', (event) => {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
+    mouse.x = event.x;
+    mouse.y = event.y;
+init(20)
+
 });
 
-// create circle
-function drawCircle(x, y, radius, color) {
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2);
-    ctx.fillStyle = color;
-    ctx.fill();
+class Particle {
+    constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 7 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = `hsl(${hue},100%, 50%)`;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if(this.size>.2) this.size -= 0.1;
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
 }
-
-// create random circle
-function randomCircle() {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    const radius = Math.random() * 50 + 10; // radius between 10 and 60
-    const color = `hsl(${Math.random() * 360}, 100%, 50%)`; // random color
-    drawCircle(x, y, radius, color);
-}
+const particles = [];
 // draw random circles
-function drawRandomCircles(count) {
+function init(count) {
     for (let i = 0; i < count; i++) {
-        randomCircle();
+         particles.push(new Particle()); 
+    }
+}
+function handleParticles() {
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+        if(particles[i].size <= 0.3){
+            particles.splice(i, 1);
+            i--;
+        }
     }
 }
 //  class particle with speed
 // Draw 100 random circles
 // animate circles with mouse movement to follow the mouse cursor
 function animateCircles() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-    drawRandomCircles(100); // draw random circles
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'; 
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // clear canvas
+    handleParticles(); // draw random circles
+hue++
     requestAnimationFrame(animateCircles); // call animateCircles again
 }
 // Start animation
